f4ba8b7144707becb33db4300445b222990190fe
Abhishek Aggarwal <aaggarwal@mozilla.com>
2023-08-04 13:34:37 +0200

    Fix installing requirements for building spambug model

--- bugbug/zenml/bugbug/bugbug/repository.py
+++ bugbug/zenml/bugbug/bugbug/repository.py
@@ -24,7 +24,7 @@ from typing import Collection, Iterable, Iterator, NewType, Optional, Set, Union
 
 import hglib
 import lmdb
-import rs_parsepatch
+#import rs_parsepatch
 import tenacity
 from tqdm import tqdm
 
@@ -730,7 +730,8 @@ def transform(hg: hglib.client, repo_dir: str, commit: Commit) -> Commit:
 
     patch = hg.export(revs=[commit.node.encode("ascii")], git=True)
     try:
-        patch_data = rs_parsepatch.get_lines(patch)
+        #patch_data = rs_parsepatch.get_lines(patch)
+        patch_data = []
     except Exception:
         logger.error(f"Exception while analyzing {commit.node}")
         raise
